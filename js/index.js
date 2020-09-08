163bd36b0a31c323656d0e2e10f3e245a34df76d
Thapelo <thapelomataboge18@gmail.com>
2020-09-08 23:07:54 +0200

    Almost finished with the look of the navbar, added social media icons

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// let date = document.querySelector('.date');
+
+
+// date.innerText = Date.getTime();
+
+// //functions
+// // function showDate() {
+// //   let now = Date.get;
+// //   console.log(now)
+// // }
